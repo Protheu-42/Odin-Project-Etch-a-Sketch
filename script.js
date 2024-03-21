56cbb232d11f244c979b0795945f16dbcc291cb5
Protheu-42 <everaldo.sobrinho42@gmail.com>
2024-03-20 21:15:58 -0300

    Button changeGrid and createRndRGBNum() create Button change amounts of squares; createRndRGBNum() use to change background of squares to random color

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const mainContainer = document.querySelector('#main-container');
 let nCol = 16; // Hard start,but latter can be change by user input
 
+// Main container function to change background color
+function createRndRGBNum (){
+    return Math.floor(Math.random() * 255);
+}
+mainContainer.addEventListener('mouseover', function(e) {
+    e.target.style.backgroundColor = `rgb(${createRndRGBNum()}, ${createRndRGBNum()}, ${createRndRGBNum()})`;
+})
+
 const flexContainer = document.createElement('div'); // work as a row
 flexContainer.className = 'flex-container';
 
@@ -25,6 +33,23 @@ function createGrid (nCol){
 
 // REMEMBER TO CALL THE FUNCTION TO CREATE THE FIRST TIME
 createGrid(nCol);
+
+// BUTTONS EVENTS
+const changeGrid = document.querySelector('#changeGrid');
+changeGrid.addEventListener('click', () => {
+    let newNCol = prompt('what is the size is this Grid? Max of 100');
+    newNCol = Number(newNCol);
+    if (Number.isInteger(newNCol) && newNCol > 1 && newNCol < 100){
+        while(mainContainer.firstChild){
+            mainContainer.removeChild(mainContainer.firstChild);
+        }
+        createGrid(newNCol);
+    }
+    else{
+        alert('bug');
+    }
+})
+
 
 
 
